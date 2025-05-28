d7a8817326cc76eee3c186c767b279f3cf58bed4
Pradeep Chandrasekar <pradeep@swargfood.com>
2025-05-28 12:46:48 +0000

    HTTP/1.1 308 Permanent Redirect location: /recipes Refresh: 0;url=/recipes Date: Wed, 28 May 2025 12:45:34 GMT Connection: keep-alive Keep-Alive: timeout=5

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,7 +1,9 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/recipes',
+  trailingSlash: true, // <--- ADD THIS LINE
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
