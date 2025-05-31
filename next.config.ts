07ff62fce4acd62e416278b49616afc4570eec6d
Pradeep Chandrasekar <pradeep@swargfood.com>
2025-05-31 13:13:38 +0000

    updated to as on server

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/recipes',
+  trailingSlash: true,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
